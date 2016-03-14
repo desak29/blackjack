e6f7e44ac5d7edafedd830ece93b5c94dea78b37
desak29 <desak29@icloud.com>
2016-03-14 11:55:58 -0400

    Blackjack (1.2)
    builds initial black jack card set up
    deal triggers dealing of cards

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 #blackjack game using jquery,html and css
+### setup the table using Bootstrap
+create a dealers ahand and players hand to hold the totals
+set each card to a custom width)adkist col-sm2 to 11% to avoid having to write 3 extra divs)
+
+add 3 buttons to the bottom of the table tso the suer can interact with teh game
+bind each button to a jquery click function
+and call correct function depending on the click
+
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
-1. deal needs to call shuffle and assign the deck
-2. set up a players starting ErrorEventHandler
-3. place cards in correct spot
+// 1. deal needs to call shuffle and assign the deck
+// 2. set up a players starting ErrorEventHandler
+// 3. place cards in correct spot
 
 
 var theDeck=[];
+var placeInDeck=0;
 
 
 $(document).ready(function(){
@@ -22,14 +23,33 @@ $(document).ready(function(){
         
     });    
       });
-      function deal(){
-          theDeck=shuffleDeck();
+      function deal()
+      {
+          shuffleDeck();
+          placeCard();
+          playerHand=[theDeck[0],theDeck[2]];
+          dealerHand=[theDeck[1],theDeck[3]];
+          placeInDeck=4;
+          placeCard(playerHand[0],'player','one');
+          placeCard(dealerHand[0],'dealer','one');
+           placeCard(playerHand[1],'player','two');
+          placeCard(dealerHand[1],'dealer','two');
+          
+          
+       
           
       }
-      function shuffleDeck(){
+      function placeCard(card,who,slot){
+          var currId='#' + who+('-card-')+slot;
+          $(currId).removeClass('empty');
+          $(currId).html(card);
+      }
+      function shuffleDeck()
+      {
         //   fill the deck in order, for now deck is made up of 52 cards 4 suits:h,s,d,call
         // s1=hearts,s2=spades,s3=diamonds,s4=clubs
-        for(s=1;s<=4;s++){
+        for(s=1;s<=4;s++)
+        {
             var suit="";
             if(s===1){
                 suit='h';
@@ -39,9 +59,23 @@ $(document).ready(function(){
                 suit="d";
             }else if(s===4){
                 suit="c";
-            }for(i=1,i<=13;i++){
+            }for(i=1;i<=13;i++)
+            {
                 theDeck.push(i+suit);
             }
       }
-      console.log(theDeck);
+            var numberOfTimesToShuffle =500;
+            for(i=1;i<numberOfTimesToShuffle;i++)
+            {
+                card1=Math.floor(Math.random()*theDeck.length);
+                card2=Math.floor(Math.random()*theDeck.length);
+                if(card1 != card2)
+                    {temp=theDeck[card1];
+                     theDeck[card1]=theDeck[card2];
+                     theDeck[card2]=temp;
+                    }
+            }
+      
+      return(theDeck);
       }
+     
